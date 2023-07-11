51474e15565e108ffba2dab4f112d6ef81493c6f
ArtJ <artiziom12@gmail.com>
2023-07-11 17:57:28 +0100

    created config file

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+    // If there's no defined constant called __CONFIG__, don't load this file
+    if(!defined("__CONFIG__")) {
+        exit("You don't have a config file");
+    }
+
+    // The config is below
+?>
