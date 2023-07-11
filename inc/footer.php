8aa3a8af8b980deb6cd3099d2063f83812b0b2eb
ArtJ <artiziom12@gmail.com>
2023-07-11 18:01:40 +0100

    created footer.php and added js footer

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<?php
+    // If there's no defined constant called __CONFIG__, don't load this file
+    if(!defined("__CONFIG__")) {
+        exit("You don't have a config file");
+    }
+
+    // The footer is below
+?>
+
+<script src="js/script.js"></script>
