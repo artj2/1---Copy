5a48201858375038819e6b916fc3c5e392d1ee4c
ArtJ <artiziom12@gmail.com>
2023-07-11 17:46:08 +0100

    Changed index.html to index.php, and added "req"

--- index.php
+++ index.php
@@ -1,15 +1,92 @@
+<?php require_once "inc/config.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Trail Hiking</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>XaAMPP </h1>
-    <?php
-    echo "<p>Today is:</p>\n";
-    echo "date : " . date('j-m-y-h-i-s');
-    ?>
+    <header>
+        <div class="navigation-bar">
+            <nav>
+                <div class="menu-icon">
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                    <div class="menu-line"></div>
+                </div>
+                <ul class="menu">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="#">Trails</a></li>
+                    <li><a href="#">Stories</a></li>
+                </ul>
+                <div class="user-options">
+                    <!-- <button class="signin-btn">Sign In</button> -->
+                    <a href="signin.html">  
+                        <button class="signin-btn">Sign In</button>  
+                    </a>
+                    <!-- <button class="register-btn">Register</button> -->
+                    <a href="register.html">  
+                        <button class="register-btn">Register</button>  
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <!-- Search Box -->
+        <form>
+            <div class="container">
+                <div class="heading">Find your perfect trail!</div>
+                <div class="search-options">
+                    <div class="search-bar">
+                        <label for="location-input">Location:</label>
+                        <input type="text" id="location-input" name="place" placeholder="Type a location...">
+                    </div>
+                    <div class="distance-range">
+                        <label for="min-length">Please enter the distance range in miles:</label>
+                        <input type="number" id="length" name="min-length">
+                        <label for="max-length">and:</label>
+                        <input type="number" id="length" name="max-length">
+                    </div>
+                    
+                    <div class="check-boxes">
+                        <div class="label">
+                            <input type="checkbox" name="dog-friendly" value="yes">
+                            Dog Friendly
+                        </div>
+                        <div class="label">
+                            <input type="checkbox" name="car-park-start" value="yes">
+                            Car Park at Start
+                        </div>
+                        <div class="label">
+                            <input type="checkbox" name="car-park-finish" value="yes">
+                            Car Park at Finish
+                        </div>
+                        <div class="label">
+                            <input type="checkbox" name="public-transport" value="yes">
+                            Public Transport
+                        </div>
+                        <div class="label">
+                            <input type="checkbox" name="food" value="yes">
+                            Food on Trail (e.g. shops)
+                        </div>
+                        <div class="label">
+                            <input type="checkbox" name="bothies" value="yes">
+                            Bothies
+                        </div>
+                    </div>
+                </div>
+                <button class="default-btn" onclick="myFunction()">Search</button>
+            </div>
+        </form>
+    </main>
+    <script src="js/script.js"></script>
 </body>
+
 </html>
---
+++ indexx.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>XaAMPP </h1>
+    <?php
+    echo "<p>Today is:</p>\n";
+    echo "date : " . date('j-m-y-h-i-s');
+    ?>
+</body>
+</html>
