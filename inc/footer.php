63699a92b2d714d071458f629d3fa0e723a5f1fe
ArtJ <artiziom12@gmail.com>
2023-07-11 19:53:35 +0100

    added jquery to footer

--- inc/footer.php
+++ inc/footer.php
@@ -7,4 +7,7 @@
     // The footer is below
 ?>
 
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <script src="js/script.js"></script>
